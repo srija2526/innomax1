30b7568869a230aca5b23d5550392efdd78e2cab
srija2526 <srijasaisrija908@gmail.com>
2026-02-27 18:15:36 +0530

    Update collegeservice.java

--- collegeservice.java
+++ collegeservice.java
@@ -1,26 +1,28 @@
-package com.tns.project.service;
+package com.example.demo.service;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.tns.project.entity.college;
-import com.tns.project.repository.collgrepo;
+import com.example.demo.entity.College;
+import com.example.demo.repository.Collgrepo;
 
 @Service
-public class collegeservice {
 
-    @Autowired
-    private collgrepo crepo;
+public class Collegeservice {
+	
+	
+	@Autowired
+    private Collgrepo crepo;
 
     // inserting the data
-    public college addcollege(college clg) {
-        return crepo.save(clg);
+    public College addcollege(College csvfile) {
+        return crepo.save(csvfile);
     }
 
     // getting the data
-    public List<college> getCollege() {
+    public List<College> getCollege() {
         return crepo.findAll();
     }
 
@@ -29,16 +31,5 @@ public class collegeservice {
         crepo.deleteById(cid);
     }
 
-    // update the data
-    public college updateCollege(college clg) {
 
-        long clgid = clg.getCid();
-        college clg1 = crepo.findById(clgid).get();
-
-        clg1.setCollegename(clg.getCollegename());
-        clg1.setCollegeadmin(clg.getCollegeadmin());
-        clg1.setLocation(clg.getLocation());
-
-        return crepo.save(clg1);
-    }
 }
